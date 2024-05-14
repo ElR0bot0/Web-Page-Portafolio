fb6724bb93cbc96880c49d937b1c38a5a035faa9
ElR0bot0 <maldonadolosadar@gmail.com>
2024-05-14 16:55:59 -0500

    First draft

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript Loaded');
-    // Add your JavaScript here
+function resizeIframe(iframe) {
+    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+}
+
+window.addEventListener('load', function() {
+    var aboutIframe = document.getElementById('about-iframe');
+    var projectsIframe = document.getElementById('projects-iframe');
+
+    aboutIframe.onload = function() {
+        resizeIframe(aboutIframe);
+    };
+
+    projectsIframe.onload = function() {
+        resizeIframe(projectsIframe);
+    };
 });
